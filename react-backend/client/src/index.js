4d4460231a1e09cf7d282e13dd8321ef00095e4b
m-lukas <lukas.mueller.gr@t-online.de>
2018-04-25 11:53:10 +0200

    Setup redux

--- react-backend/client/src/index.js
+++ react-backend/client/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, Route, Link } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import 'semantic-ui-css/semantic.min.css';
 
@@ -8,16 +12,20 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import Project from './components/Project';
 import LoginPage from './components/pages/LoginPage';
+import rootReducer from './rootReducer';
 
 const root = document.getElementById('root');
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
     <HashRouter>
-        <div>
-            <Route exact path="/" component={App} />
-            <Route path="/projects/:project" component={Project} />
-            <Route path="/login" component={LoginPage} /> 
-        </div>
+        <Provider store={store}>
+            <div>
+                <Route exact path="/" component={App} />
+                <Route path="/projects/:project" component={Project} />
+                <Route path="/login" component={LoginPage} /> 
+            </div>
+        </Provider>
     </HashRouter>
 , root);
 registerServiceWorker();
