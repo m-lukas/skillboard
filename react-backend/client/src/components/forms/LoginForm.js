6f2670222a360b6145797957e37e971a423442ad
m-lukas <lukas.mueller.gr@t-online.de>
2018-04-25 11:08:29 +0200

    Finish login form

--- react-backend/client/src/components/forms/LoginForm.js
+++ react-backend/client/src/components/forms/LoginForm.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { Form, Button } from 'semantic-ui-react';
+import Validator from 'validator';
+import PropTypes from 'prop-types';
+
+import InlineError from '../messages/InlineError';
 
 class LoginForm extends Component {
   state = {
@@ -8,11 +12,30 @@ class LoginForm extends Component {
     errors: {}
   }
 
+  onChange = e => this.setState({ 
+    data: { ...this.state.data, [e.target.name]: e.target.value } 
+  });
+
+  onSubmit = e => {
+    const errors = this.validate(this.state.data);
+    this.setState({ errors });
+    if(Object.keys(errors).length === 0){
+      this.props.submit(this.state.data);
+    }
+  };
+
+  validate = (data) => {
+    const errors = {};
+    if(!Validator.isEmail(data.email)) errors.email = "Invalid email";
+    if(!data.password) errors.password = "Can't be blank";
+    return errors;
+  }
+
   render() {
-    const { data } = this.state;
+    const { data, errors } = this.state;
     return (
-        <Form>
-          <Form.Field>
+        <Form onSubmit={this.onSubmit}>
+          <Form.Field error={!!errors.email}>
             <label htmlFor='email'>Email</label>
             <input 
               type='email' 
@@ -22,11 +45,28 @@ class LoginForm extends Component {
               value={data.email}
               onChange={this.onChange}
             />
+            { errors.email && <InlineError text={errors.email} /> }
+          </Form.Field>
+          <Form.Field error={!!errors.password}>
+            <label htmlFor='password'>Password</label>
+            <input 
+              type='password' 
+              id='password' 
+              name='password' 
+              placeholder='Secure password'
+              value={data.password}
+              onChange={this.onChange}
+            />
+            { errors.password && <InlineError text={errors.password} /> }
           </Form.Field>
           <Button primary>Login</Button>
         </Form>
     );
   }
+}
+
+LoginForm.propTypes = {
+  submit: PropTypes.func.isRequired
 }
 
 export default LoginForm;
--- react-backend/client/src/components/pages/LoginPage.js
+++ react-backend/client/src/components/pages/LoginPage.js
@@ -3,12 +3,16 @@ import LoginForm from '../forms/LoginForm';
 
 class LoginPage extends Component {
 
+  submit = data => {
+    console.log(data);
+  }
+
   render() {
 
     return (
         <div>
             <h1>Login Page</h1>
-            <LoginForm />
+            <LoginForm submit={this.submit} />
         </div>
     );
   }
