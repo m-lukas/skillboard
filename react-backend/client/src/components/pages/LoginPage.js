63361fa4315964535d943872b1b5ade980b3fab6
m-lukas <lukas.mueller.gr@t-online.de>
2018-04-25 13:23:26 +0200

    Initialize auth structure

--- react-backend/client/src/components/pages/LoginPage.js
+++ react-backend/client/src/components/pages/LoginPage.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import LoginForm from '../forms/LoginForm';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { login } from '../../actions/auth';
 
 class LoginPage extends Component {
 
-  submit = data => {
-    console.log(data);
-  }
+  submit = data => this.props.login(data).then(() => this.props.history.push("/"));
 
   render() {
 
@@ -18,4 +19,11 @@ class LoginPage extends Component {
   }
 }
 
-export default LoginPage;
+LoginPage.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired
+  }).isRequired,
+  login: PropTypes.func.isRequired
+};
+
+export default connect(null, { login })(LoginPage);
